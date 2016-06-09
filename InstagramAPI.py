5ee5f4d0120688d4229e466621e6a93427504ba0
vladimir-bezrukov <web.dev903@gmail.com>
2016-06-09 21:55:03 +0300

    Update InstagramAPI.py

--- InstagramAPI.py
+++ InstagramAPI.py
@@ -482,6 +482,6 @@ class InstagramAPI:
 InstagramAPI = InstagramAPI("login", "password")
 InstagramAPI.login() # login
 InstagramAPI.tagFeed("cat") # get media list by tag #cat
-media_id = InstagramAPI.LastJson # media id of first media
+media_id = InstagramAPI.LastJson # last response JSON
 InstagramAPI.like(media_id["ranked_items"][0]["pk"]) # like first media
 InstagramAPI.getUserFollowers(media_id["ranked_items"][0]["user"]["pk"]) # get first media owner followers
