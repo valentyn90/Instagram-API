4efb6bbec40cb7dc2f3506dba5decc0342f4c0ee
xecgr <francesc.garcia.robert@gmail.com>
2017-05-10 17:20:34 +0200

    Make installable

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='instagram-api',
+    version='0.1',
+    description='Unofficial instagram API, give you access to ALL instagram features (like, follow, upload photo and video and etc)! Write on python.',
+    url='https://github.com/LevPasha/Instagram-API-python/',
+    author='Pasha Lev',
+    author_email='levpasha@gmail.com',
+    license='GNU',
+    packages=['instagram_api'],
+    zip_safe=False,
+    install_requires=[
+      "requests==2.11.1",
+      "requests-toolbelt==0.7.0",
+    ]
+)
