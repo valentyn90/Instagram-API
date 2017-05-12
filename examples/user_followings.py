c1809c917a08d4f46375fce171ac5bbb43061dbc
xecgr <francesc.garcia.robert@gmail.com>
2017-05-12 14:15:46 +0200

    user_followings fixes

--- examples/user_followings.py
+++ examples/user_followings.py
@@ -4,11 +4,11 @@ from datetime import datetime
 
 username = ''
 pwd = ''
-
+user_id  = ''
 
 API = InstagramAPI(username,pwd)
 API.login()
-user_id  = ''
+
 API.getUsernameInfo(user_id)
 API.LastJson
 following   = []
@@ -17,7 +17,7 @@ while next_max_id:
     print next_max_id
     #first iteration hack
     if next_max_id == True: next_max_id=''
-    _ = API.getUserFollowers(user_id,maxid=next_max_id)
+    _ = API.getUserFollowings(user_id,maxid=next_max_id)
     following.extend ( API.LastJson.get('users',[]))
     next_max_id = API.LastJson.get('next_max_id','')
 
